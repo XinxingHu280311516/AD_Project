1938a0ab3ac7e76159d952b97cd52a4399c7b8af
JKnightY <E1133134@u.nus.edu>
2024-08-05 17:14:01 +0800

    类别的增删改

--- app/src/main/java/com/example/adproject/ApiService.java
+++ app/src/main/java/com/example/adproject/ApiService.java
@@ -13,7 +13,6 @@ import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Part;
 import retrofit2.http.Path;
-import retrofit2.http.Query;
 
 public interface ApiService {
     @POST("login")
